13222c159260111356923d2f556b4f8c7761a61a
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-24 17:31:34 +0200

    updated documentation

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -11,12 +11,12 @@ define(['js/form', 'form/util', 'globalize'], function(Form, Util) {
     var changeLanguage = function(language) {
         require(['cultures/globalize.culture.' + language], function() {
             Globalize.culture(language);
-            form.validation.validate(true);
+            //form.validation.validate(true);
         }.bind(this));
     };
 
     var language = 'de';
     changeLanguage(language);
 
-    var form = new Form($('#content'));
+    //var form = new Form($('#content'));
 });
