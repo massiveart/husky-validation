847d23a343657f50dba96739413b833d70ad49e3
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-30 18:09:59 +0200

    fixed merge conflicts

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,55 +5,77 @@ module.exports = function(grunt) {
     // load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    var requireJS = {
-        baseUrl: '.',
-        preserveLicenseComments: false,
-        paths: {
-            'validation': 'js/validation',
-            'validation-element': 'js/element',
+    var clone = grunt.util._.clone,
+        requireJS = {
+            baseUrl: '.',
+            preserveLicenseComments: false,
+            paths: {
+                'form': 'js/form',
+                'form/mapper': 'js/mapper',
+                'form/validation': 'js/validation',
+                'form/element': 'js/element',
+                'form/util': 'js/util',
 
-            'type/default': 'js/types/default',
-            'type/date': 'js/types/date',
-            'type/decimal': 'js/types/decimal',
-            'type/email': 'js/types/email',
-            'type/url': 'js/types/url',
+                'type/default': 'js/types/default',
+                'type/string': 'js/types/string',
+                'type/date': 'js/types/date',
+                'type/decimal': 'js/types/decimal',
+                'type/email': 'js/types/email',
+                'type/url': 'js/types/url',
+                'type/label': 'js/types/label',
 
-            'validator/default': 'js/validators/default',
-            'validator/min': 'js/validators/min',
-            'validator/max': 'js/validators/max',
-            'validator/minlength': 'js/validators/minlength',
-            'validator/maxlength': 'js/validators/maxlength',
-            'validator/required': 'js/validators/required'
+                'validator/default': 'js/validators/default',
+                'validator/min': 'js/validators/min',
+                'validator/max': 'js/validators/max',
+                'validator/minLength': 'js/validators/min-length',
+                'validator/maxLength': 'js/validators/max-length',
+                'validator/required': 'js/validators/required',
+                'validator/unique': 'js/validators/unique',
+
+                'globalize': 'bower_components/globalize/lib/globalize',
+                'cultures': 'bower_components/globalize/lib/cultures'
+            },
+            include: [
+                'form',
+
+                'type/string',
+                'type/date',
+                'type/decimal',
+                'type/email',
+                'type/url',
+                'type/label',
+
+                'validator/min',
+                'validator/max',
+                'validator/minLength',
+                'validator/maxLength',
+                'validator/required',
+                'validator/unique'
+            ],
+            out: 'dist/validation.js',
+            optimize: "uglify"
         },
-        include: [
-            'validation',
-
-            'type/default',
-            'type/date',
-            'type/decimal',
-            'type/email',
-            'type/url',
-
-            'validator/default',
-            'validator/min',
-            'validator/max',
-            'validator/minlength',
-            'validator/maxlength',
-            'validator/required'
-        ],
-        out: 'dist/validation.js'
-    };
+        getOptions = function(dev) {
+            var _s = clone(requireJS);
+            _s.out = 'dist/validation' + (dev ? '' : '.min') + ".js";
+            _s.optimize = (dev ? 'none' : 'uglify');
+            return _s;
+        };
 
     // project configuration
     grunt.initConfig({
         requirejs: {
-            validation: {
-                options: requireJS
+            dist: {
+                options: getOptions(false)
+            },
+            dev: {
+                options: getOptions(true)
             }
         }
     });
 
     grunt.registerTask('build', [
-        'requirejs:validation'
+        'requirejs:dist',
+        'requirejs:dev'
     ]);
 };
