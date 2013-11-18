d9ad6191124eb91d202ddada0e0a6a718fc716cb
Johannes Wachter <johannes.wachter@massiveart.com>
2013-11-18 11:13:25 +0100

    build new version

--- dist/validation.js
+++ dist/validation.js
@@ -1371,11 +1371,15 @@ define('type/label',[
             typeInterface = {
                 setValue: function(value) {
                     if (!!value[this.options.label]) {
-                        var label = value[this.options.label];
+                        var label = value[this.options.label],
+                            labelValue = value[this.options.label];
                         if (!!this.options.translate) {
-                            label = Globalize.localize(label, Globalize.culture().name);
+                            labelValue = Globalize.localize(label, Globalize.culture().name);
+                            if (labelValue === undefined) {
+                                labelValue = label;
+                            }
                         }
-                        this.$el.text(label);
+                        this.$el.text(labelValue);
                     }
 
                     if (!!value[this.options.id]) {
