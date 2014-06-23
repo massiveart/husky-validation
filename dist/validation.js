1e7434fa6f70372d37298d68eb6be9608498b821
Johannes Wachter <johannes.wachter@massiveart.com>
2014-06-23 13:04:29 +0200

    added build files

--- dist/validation.js
+++ dist/validation.js
@@ -1,4 +1,3 @@
-
 /*
  * This file is part of the Husky Validation.
  *
@@ -1708,6 +1707,7 @@ define('type/decimal',[
 
     return function($el, options) {
         var defaults = {
+                format: 'n', // n, d, c, p
                 regExp: /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/
             },
 
@@ -1723,6 +1723,17 @@ define('type/decimal',[
                     }
 
                     return this.options.regExp.test(val);
+                },
+
+                getModelData: function(val) {
+                    return Globalize.parseFloat(val);
+                },
+
+                getViewData: function(val) {
+                    if(typeof val === 'string'){
+                        val = parseFloat(val);
+                    }
+                    return Globalize.format(val, this.options.format);
                 }
             };
 
@@ -2630,3 +2641,4 @@ define('validator/regex',[
     };
 
 });
+
